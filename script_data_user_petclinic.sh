cd3e832de0dcab31f0a2cea29d1fa8b2c0855f24
Amioss <125217466+Amioss@users.noreply.github.com>
2023-04-02 11:06:48 +0200

    update user data

--- script_data_user_petclinic.sh
+++ script_data_user_petclinic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get -y install docker.io
-sudo docker pull amioss/app_petclinic:latest
-sudo docker run -d --name appli -p 80:8080 amioss/app_petclinic:latest
+sudo docker pull amioss/app_petclinic:1.0
+sudo docker run -d --name appli -p 80:8080 amioss/app_petclinic:1.0
